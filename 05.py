f3a1b1893868dab4ad118664c7b509395e34a356
Jatin Gandhi <gandhijatink@gmail.com>
2025-02-04 17:17:27 +0530

    Click signal connected to local slot function.

---
+++ 05.py
@@ -0,0 +1,32 @@
+import sys;
+from PyQt6.QtWidgets import QApplication;
+from PyQt6.QtWidgets import QMainWindow;
+from PyQt6.QtWidgets import QPushButton;
+
+
+class MainWindow(QMainWindow):
+    count = 0;
+    def __init__(self):
+        self.count = 0;
+        super().__init__();
+        self.setWindowTitle("Test App");
+        self.setFixedSize(720,480);
+
+        button = QPushButton("Press it!");
+        button.setCheckable(True);
+        button.clicked.connect(self.handleBtnPress);
+
+        self.setCentralWidget(button);
+
+    def handleBtnPress(self):
+        self.count = self.count + 1;
+        self.setWindowTitle("Test App: {}".format(self.count));
+        print("Button pressed!!");
+
+
+app = QApplication(sys.argv);
+
+win = MainWindow();
+win.show();
+
+app.exec();
