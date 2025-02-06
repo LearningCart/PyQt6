b2d83e78b69221e74c94cc87e0cc56c956913fe2
Jatin Gandhi <gandhijatink@gmail.com>
2025-02-06 23:17:26 +0530

    Show/Hide window

--- 29.py
+++ 29.py
@@ -1,15 +1,3 @@
-# Persistent Window
-"""
-So far we've looked at how to create new windows on demand. 
-However, sometimes you have a number of standard application windows. 
-In this case rather than create the windows when you want to show them, 
-it can often make more sense to create them at start-up, then use .show() 
-to display them when needed.
-
-In the following example we create our external window in the __init__ 
-block for the main window, and then our show_new_window method simply 
-calls self.w.show() to display it.
-"""
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
 
 import sys
@@ -31,16 +19,18 @@ class AnotherWindow(QWidget):
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.w = AnotherWindow()
         self.button = QPushButton("Push for Window")
-        self.button.clicked.connect(self.show_new_window)
+        self.button.clicked.connect(self.toggle_window)
         self.setCentralWidget(self.button)
 
-    def show_new_window(self, checked):
-        self.w.show()
+    def toggle_window(self, checked):
+        if self.w.isVisible():
+            self.w.hide()
+        else:
+            self.w.show()
 
 
 app = QApplication(sys.argv)
