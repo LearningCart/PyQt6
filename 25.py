5e54244cdfe5233bd42f5d3403fbcb951c3d63bc
Jatin Gandhi <gandhijatink@gmail.com>
2025-02-06 22:48:44 +0530

    Custom diaglog

--- 25.py
+++ 25.py
@@ -1,8 +1,27 @@
 # QDialog
 import sys
+from PyQt6.QtWidgets import (QVBoxLayout);
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QLabel, QDialogButtonBox
 
-from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
+class CustomDialog(QDialog):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("HELLO!")
+
+        QBtn = (
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        layout.addWidget(message)
+        layout.addWidget(self.buttonBox)
+        self.setLayout(layout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,12 +33,20 @@ class MainWindow(QMainWindow):
         button.clicked.connect(self.button_clicked);
         self.setCentralWidget(button);
 
-    def button_clicked(self, s):
-        print("click", s);
+    # def button_clicked(self, s):
+    #     print("click", s);
 
-        dlg = QDialog(self);
-        dlg.setWindowTitle("Hollow World!");
-        dlg.exec();
+    #     dlg = QDialog(self);
+    #     dlg.setWindowTitle("Hollow World!");
+    #     dlg.exec();
+
+    def button_clicked(self, s):
+        print("click", s)
+        dlg = CustomDialog()
+        if dlg.exec():
+            print("Success!")
+        else:
+            print("Cancel!")
 
 
 app = QApplication(sys.argv)
